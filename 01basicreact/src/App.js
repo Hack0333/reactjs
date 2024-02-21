a7e245fc1accebe3f78dc9559a26efaa2fa3cd30
Hack0333 <absharma0333@gmail.com>
2024-02-21 10:18:02 +0530

    video 3rd

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+   <h1>deepak react | deepak Kumar sharma
+    preview of the core react structure
+   </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <h1>react with vite | 
+      deepak Kumar Sharma 
+      LEarning the structure of react with vite
+    </h1>
+  )
+}
+
+export default App
