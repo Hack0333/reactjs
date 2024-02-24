169b6314637a5a3308d80912fbfc04d765a8e75a
Hack0333 <absharma0333@gmail.com>
2024-02-24 19:16:43 +0530

    custom dom

--- 01vitereact/src/Deepak.jsx
+++ 01vitereact/src/Deepak.jsx
@@ -1,3 +1,5 @@
+// keep the function name capital 
+//also try to write the file name in capital it's a best parctise
 function Deepak(){
     return(
         <h1>First vite function</h1>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <h1>Custom !!</h1>
+    )
+}
+
+// custom react
+const ReactElement = {
+    type : 'a',
+    props : {
+        href : "https://google.com",
+        target : "_blank"
+    },
+    children : "click me to visit",
+}
+
+const anotherElement = (
+    <a href='www.google.com' target='_blank'>visit me</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    // <MyApp />
+    // MyApp() it will also run because end of the day its also a funtion
+    //reactElement //object is used with parenthesis
+    anotherElement
 
 )
