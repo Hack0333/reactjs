d1e4548db7abc80b62bd2b684c8e0d9b4f42147b
Hack0333 <absharma0333@gmail.com>
2024-03-04 10:53:19 +0530

    before 12th project

---
+++ 11ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Addtodo from './components/Addtodo'
+import Todo from './components/Todo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <Addtodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
